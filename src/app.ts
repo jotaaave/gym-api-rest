df3bbdc80faf05f2d1c5a3ad15e6901f84839303
jotaaave <marbenebela@gmail.com>
2025-12-05 15:35:10 -0300

    :sparkles: add route create user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify"
-import { PrismaClient } from "./generated/prisma/client.js"
+import { appRoutes } from "./http/routes/routes.js"
 
 export const app = fastify()
 
-const prisma = new PrismaClient({accelerateUrl: ''})
-
-prisma.user.create({
-    data: {
-        email: 'eere@gmail.com',
-        name: 'joao'
-    }
-})
+app.register(appRoutes)
---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { env } from "@/env/index.js"
+import { PrismaClient } from "@/generated/prisma/client.js"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const connectionString = env.DATABASE_URL
+const adapter = new PrismaPg({ connectionString })
+export const prisma = new PrismaClient({ adapter })
