b65f6c9dea9cf4f6235ac83bfa837286847730f9
jotaaave <marbenebela@gmail.com>
2025-12-05 17:16:48 -0300

    feat (user): implement user registration feature

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,4 +1,5 @@
-import { InMemoryUsersRepository } from "@/repositories/memory/in-memory-users-repository.js"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository.js"
+import { UserAlreadyExistsError } from "@/services/errs/user-already-exists-err.js"
 import { RegisterService } from "@/services/users/register-service.js"
 import type { FastifyReply, FastifyRequest } from "fastify"
 import z from "zod"
@@ -13,12 +14,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const { name, email, password } = registerBodySchema.parse(request.body)
 
 	try {
-		const repository = new InMemoryUsersRepository()
+		const repository = new PrismaUsersRepository()
 		const registerService = new RegisterService(repository)
 		await registerService.execute({ name, email, password })
+
 	} catch (err) {
-		console.log(err)
-		return reply.status(409).send()
+		if (err instanceof UserAlreadyExistsError) {
+			return reply.status(409).send({ message: err.message })
+		}
+
+		return reply.status(500).send() // TODO: Fix this
 	}
 
 	reply.status(201).send()
